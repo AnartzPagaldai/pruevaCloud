b99f9a795d325c0d34165212d5820626d54797c6
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-01-15 09:49:05 +0100

    ultimo cambio

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     require "db.php";
     $id = select("SELECT id FROM users WHERE name = :nombre AND pass = :pass",
     ["nombre" => $_POST["nombre"], "pass" => $_POST["pass"]]);
-    if ($id[0]) { 
+    if (isset($id[0])) { 
         die("iniciado");
     } else {
         $error = "incorrecto";
