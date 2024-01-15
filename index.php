51f8ed74937e15d28c3e8a006d09c12ee961ce57
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-01-15 09:29:23 +0100

    db

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-require "db.php";
-
 $error = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    require "db.php";
     $id = select("SELECT id FROM users WHERE nombre = :nombre AND pass = :pass",
     ["nombre" => $_POST["nombre"], "pass" => $_POST["pass"]]);
     if ($id[0]) { 
