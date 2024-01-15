c898f754b1a0a7ebed3beda5379e2b99fa4fb67d
Anartz <anartz.pagaldai@ikasle.egibide.org>
2024-01-15 09:41:39 +0100

    cambio env

--- db.php
+++ db.php
@@ -4,13 +4,20 @@ $conn = connect();
 function connect()
 {
     global $conn;
+    /*
     $host = "viaduct.proxy.rlwy.net";
     $dbname = "railway";
     $user = "root";
     $password = "GB6aDEdG1H6bHfFfEhAD3CGd6ga-E21-";
-
+    */
+    define('DB_HOST', 'viaduct.proxy.rlwy.net:26459');
+    define('DB_NAME', 'railway');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'GB6aDEdG1H6bHfFfEhAD3CGd6ga-E21-');
+    
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        //$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
         return $conn;
     } catch (PDOException $e) {
         echo $e->getMessage();
